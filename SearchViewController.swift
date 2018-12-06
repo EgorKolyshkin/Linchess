7524aa3fb821d362a76eb6474158a64e537abc3c
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2018-12-07 01:16:52 +0300

    Table View for Search VC

--- SearchViewController.swift
+++ SearchViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SearchViewController: UIViewController, UISearchResultsUpdating {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     func updateSearchResults(for searchController: UISearchController) {
         print(1)
     }
@@ -17,6 +19,8 @@ class SearchViewController: UIViewController, UISearchResultsUpdating {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         setUpNavigationBar()
     }
     
@@ -30,6 +34,7 @@ class SearchViewController: UIViewController, UISearchResultsUpdating {
         self.navigationItem.hidesSearchBarWhenScrolling = false
 
     }
+    
     /*
     // MARK: - Navigation
 
@@ -40,4 +45,27 @@ class SearchViewController: UIViewController, UISearchResultsUpdating {
     }
     */
 
+}
+
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        cell.detailTextLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
